90a6039603c569bb502d68a3b289b4e459e4756e
Genaro V Salinas <gvsalinas3@gmail.com>
2021-06-04 00:18:41 -0500

    Added user filtering state management

--- client/components/LeaderBoard.jsx
+++ client/components/LeaderBoard.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import { Page, Header } from '../styles/BoardStyles.js';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Page } from '../styles/BoardStyles.js';
 import ArtistItem from './ArtistItem.jsx';
+import ArtistsNav from './ArtistsNav.jsx';
+
+const filterHandler = (artists = []) => {
+  let artists = artists;
+  return function(query) {
+    let { search, category } = query;
+    if (!search && !category) return artists;
+  }
+}
 
 const LeaderBoard = ({ board }) => {
+  const [search, setSearch] = useState('');
+  const [category, setCategory] = useState('');
+  const filter = filterHandler(board);
+  let artists = filter({ search, category })
+  console.log('Search: ', search);
+  console.log('Category: ', category);
+
+
   return (
     <Page>
-      <Header>
-        <div style={{gridArea: '1/3/1/4', textAlign: 'center'}}>Artist</div>
-        <div style={{gridArea: '1/4/1/5', textAlign: 'center'}}>Track</div>
-        <div style={{gridArea: '1/5/1/6', textAlign: 'center'}}>Likes</div>
-      </Header>
-      {board.map((artist, i) => {
+      <ArtistsNav setSearch={setSearch} setCategory={setCategory}/>
+      {artists.map((artist, i) => {
         return <ArtistItem
-                  key={i}
+                  key={uuidv4()}
                   idx={i}
                   artist={artist} />
       })}
