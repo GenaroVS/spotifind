8d810cf0ddb41fa0a866be2778804b0ff79f8950
Genaro V Salinas <gvsalinas3@gmail.com>
2021-06-08 00:35:14 -0500

    today component

--- client/components/Today.jsx
+++ client/components/Today.jsx
@@ -1,45 +1,9 @@
-import React, { useState, useContext } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Chat from './Chat.jsx';
 import ArtistBanner from './ArtistBanner.jsx';
 import { Page } from '../styles/TodayStyles.js';
-import { AuthContext } from '../utils/context.js';
 
 export default ({ artist, liked, setLiked }) => {
-  const user = useContext(AuthContext);
-  const [hasFav, setFavoriteRes] = useState('');
-
-
-  function like() {
-    var popup = document.getElementById('popup-liked');
-    popup.classList.toggle('open');
-
-    setTimeout(() => {
-      popup.classList.toggle('open');
-    }, 2000);
-
-    if (!liked) {
-      axios.put(`/auth/newLike/${artist.id}`)
-        .then(res => setLiked(!liked))
-        .catch(err => console.error(err));
-    }
-  }
-
-  function favorite() {
-    var popup = document.getElementById('popup-fav');
-    popup.classList.toggle('open');
-    setTimeout(() => {
-      popup.classList.toggle('open');
-    }, 2000);
-
-
-    axios.post('/auth/favorites', {
-      userId: user.sub,
-      artistId: artist.id
-    })
-      .then(res => setFavoriteRes(res.data))
-      .catch(err => console.error(err));
-  }
 
   if (!artist.artist_photo) {
     return (
@@ -52,8 +16,7 @@ export default ({ artist, liked, setLiked }) => {
   return (
     <Page>
       <ArtistBanner
-        like={like}
-        favorite={favorite}
+        setLiked={setLiked}
         artist={artist}
         liked={liked}/>
       <Chat />
